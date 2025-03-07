a3359b5be886685e7b3167dcd8508bdcd293678f
Danil Sveshnikov <bloodshoww@mail.ru>
2025-03-07 14:39:52 +0300

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <script setup lang="ts">
 
+import Header from '@/layouts/Header.vue'
 </script>
 
 <template>
-
-  <h1>Hello World!</h1>
-
+  <div class="wrapper">
+    <Header/>
+    <div class="content">
+      <h1>Hello World!</h1>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style>
+  .wrapper {
+    width: 90vw;
+    height: 100%;
+    margin: 50px auto;
+    border-radius: 10px;
+    background-color: #f6f6f6;
+    box-shadow: 0 15px 20px 0 rgba(0, 0, 0, 0.03);
+  }
 
+  .content {
+    padding: 40px 60px;
+  }
 </style>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="button">
+    <slot></slot>
+  </button>
+</template>
+
+<style>
+  .button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    padding: 15px 23px;
+    width: 100%;
+    max-width: 150px;
+
+    font-family: var(--font-family);
+    font-weight: 700;
+    font-size: 16px;
+    color: #fff;
+
+    color: var(--color-gray-line);
+    border-radius: 30px;
+    border: none;
+    background-color: var(--color-orange);
+    cursor: pointer;
+
+
+  }
+</style>
